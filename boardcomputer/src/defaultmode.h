11f04064dfba5402cc4a823a55e9f1a56bbe9133
0xFEEDC0DE64 <daniel@brunner.ninja>
2020-02-14 20:57:13 +0100

    minor changes

--- boardcomputer/src/defaultmode.h
+++ boardcomputer/src/defaultmode.h
@@ -14,6 +14,9 @@ public:
     void update() override;
     void stop() override {};
 
+    bool enableWeakeningSmoothening{true};
+    int16_t weakeningSmoothening{1};
+    float lastPwm{0};
     int16_t frontPercentage{100}, backPercentage{100};
     bool waitForGasLoslass{false};
     bool waitForBremsLoslass{false};
