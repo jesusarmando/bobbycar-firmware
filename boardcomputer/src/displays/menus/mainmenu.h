e8726dbdf6e7a5149338a67118e4aeafc606bf2a
0xFEEDC0DE64 <daniel@brunner.ninja>
2020-04-18 04:04:18 +0200

    Icons

--- boardcomputer/src/displays/menus/mainmenu.h
+++ boardcomputer/src/displays/menus/mainmenu.h
@@ -6,12 +6,15 @@
 #include "actions/switchscreenaction.h"
 #include "actions/rebootaction.h"
 #include "texts.h"
+#include "icons/back.h"
+#include "icons/modes.h"
+#include "icons/presets.h"
 #include "icons/graph.h"
 #include "icons/settings.h"
 #include "icons/lock.h"
+#include "icons/demos.h"
 #include "icons/poweroff.h"
 #include "icons/reboot.h"
-#include "icons/back.h"
 
 namespace {
 class StatusDisplay;
@@ -31,12 +34,12 @@ class MainMenu :
     public StaticText<TEXT_MAINMENU>,
     public StaticMenuDefinition<
         makeComponent<MenuItem, StaticText<TEXT_STATUS>,      SwitchScreenAction<StatusDisplay>, StaticMenuItemIcon<&icons::back>>,
-        makeComponent<MenuItem, StaticText<TEXT_SELECTMODE>,  SwitchScreenAction<SelectModeMenu>>,
-        makeComponent<MenuItem, StaticText<TEXT_PRESETS>,     SwitchScreenAction<PresetsMenu>>,
+        makeComponent<MenuItem, StaticText<TEXT_SELECTMODE>,  SwitchScreenAction<SelectModeMenu>, StaticMenuItemIcon<&icons::modes>>,
+        makeComponent<MenuItem, StaticText<TEXT_PRESETS>,     SwitchScreenAction<PresetsMenu>, StaticMenuItemIcon<&icons::presets>>,
         makeComponent<MenuItem, StaticText<TEXT_GRAPHS>,      SwitchScreenAction<GraphsMenu>, StaticMenuItemIcon<&icons::graph>>,
         makeComponent<MenuItem, StaticText<TEXT_SETTINGS>,    SwitchScreenAction<SettingsMenu>, StaticMenuItemIcon<&icons::settings>>,
         makeComponent<MenuItem, StaticText<TEXT_LOCKVEHICLE>, SwitchScreenAction<Lockscreen>, StaticMenuItemIcon<&icons::lock>>,
-        makeComponent<MenuItem, StaticText<TEXT_DEMOS>,       SwitchScreenAction<DemosMenu>>,
+        makeComponent<MenuItem, StaticText<TEXT_DEMOS>,       SwitchScreenAction<DemosMenu>, StaticMenuItemIcon<&icons::demos>>,
         makeComponent<MenuItem, StaticText<TEXT_POWEROFF>,    SwitchScreenAction<PoweroffDisplay>, StaticMenuItemIcon<&icons::poweroff>>,
         makeComponent<MenuItem, StaticText<TEXT_REBOOT>,      RebootAction, StaticMenuItemIcon<&icons::reboot>>,
         makeComponent<MenuItem, StaticText<TEXT_DEBUG>,       SwitchScreenAction<DebugMenu>>
